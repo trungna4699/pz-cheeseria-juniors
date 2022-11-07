2b2a5f95a0a83fa75d972072eaab4585afee23ac
trungna4699 <trungna4699@gmail.com>
2022-11-07 12:05:09 +1000

    fix POST request by adding jsonParser

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -1,5 +1,10 @@
 import * as express from 'express';
 
+const bodyParser = require('body-parser')
+
+// create application/json parser
+const jsonParser = bodyParser.json()
+
 // Types
 export type CartItemType = {
     id: number;
@@ -11,23 +16,23 @@ export type CartItemType = {
     amount: number;
   };
 
-const purchasedItems: CartItemType[] = [];
-// console.log("Items purchased here: " + purchasedItems)
-// console.log(purchasedItems);
+let purchasedItems: CartItemType[] = [];
 
 const cheeses = require('./data/cheeses.json');
 const router = express.Router();
 
 router.get('/api/cheeses', (req, res, next) => {
-
     res.json(cheeses);
 });
 
-router.post('/api/purchases', (req, res, next) => {
-    // console.log(req.body);
+router.post('/api/purchases', jsonParser, (req, res, next) => {
     purchasedItems.push(req.body);
-    // console.log(purchasedItems);
-    // res.sendStatus(200);
+    console.log("post received: ", purchasedItems);
+});
+
+router.get('/api/purchases', (req, res, next) => {
+    console.log("getPurchases reached!!!");
+    res.json(purchasedItems);
 });
 
 export default router;
